cc66c8bfe51f308011b2cf759bfdf1da075c94bb
SoloFriend <shengyt9331@126.com>
2016-08-30 17:51:15 +0800

    Update SortUtils.java
    修改快速排序代码

--- SortUtils.java
+++ SortUtils.java
@@ -55,40 +55,44 @@ class SortUtils{
 			numbers[position] = temp;
 		}
  	}
-/**  
- * 快速排序
- * 
- * 从数列中挑出一个元素，称为“基准”  
- * 重新排序数列，所有元素比基准值小的摆放在基准前面，所有元素比基准值大的摆在基准的后面（相同的数可以到任一边）。在这个分割之后，
-   该基准是它的最后位置。这个称为分割（partition）操作。
- * 递归地把小于基准值元素的子数列和大于基准值元素的子数列排序。  
- *   
- * @param numbers  待排序数组
- * @param start  起始位置
- * @param end  结束位置
- */  
-public static void quickSort(int[] numbers, int start, int end) {   
-    if (start < end) {   
-        int base = numbers[start]; // 选定的基准值（第一个数值作为基准值）   
-        int temp; // 记录临时中间值   
-        int i = start, j = end;   
-        do {   
-            while ((numbers[i] < base) && (i < end))   
-                i++;   
-            while ((numbers[j] > base) && (j > start))   
-                j--;   
-            if (i <= j) {   
-                temp = numbers[i];   
-                numbers[i] = numbers[j];   
-                numbers[j] = temp;   
-                i++;   
-                j--;   
-            }   
-        } while (i <= j);   
-        if (start < j)   
-            quickSort(numbers, start, j);   
-        if (end > i)   
-            quickSort(numbers, i, end);   
-    }   
-}  
+	/**  
+ 	* 快速排序
+ 	* 
+ 	* 从数列中挑出一个元素，称为“基准”  
+ 	* 重新排序数列，所有元素比基准值小的摆放在基准前面，所有元素比基准值大的摆在基准的后面（相同的数可以到任一边）。在这个分割之后，
+   	  该基准是它的最后位置。这个称为分割（partition）操作。
+ 	* 递归地把小于基准值元素的子数列和大于基准值元素的子数列排序。  
+ 	*   
+ 	* @param numbers  待排序数组
+ 	* @param start  数组起始位置
+ 	* @param end  数组结束位置
+ 	*/  
+	public static void quickSort(int[] numbers, int start, int end) {   
+    	if (start < end) {   
+        	int base = numbers[start]; // 选定的基准值（第一个数值作为基准值）   
+        	int temp; // 记录临时中间值   
+        	int startIndex = start //定义起始索引
+        	int endIndex = end;   //定义结尾索引
+        	do { 
+        		/*如果起始索引值小于基准值，并且起始索引小于结束位置，起始索引循环后移*/
+         		while ((numbers[startIndex] < base) && (startIndex < end))   
+            	    startIndex++;
+            	/*如果结尾索引值大于基准值，并且结尾索引大于起始位置，结尾索引循环前移*/
+            	while ((numbers[endIndex] > base) && (endIndex > start))   
+                	endIndex--;
+                /*如果起始索引小于等于结尾索引，交换索引值，并且起始索引后移，结束索引前移*/
+            	if (startIndex <= endIndex) {   
+                	temp = numbers[startIndex];   
+             		numbers[startIndex] = numbers[endIndex];   
+                	numbers[endIndex] = temp;   
+                	startIndex++;   
+                	endIndex--;   
+            	}   
+        	} while (startIndex <= endIndex);   
+        	if (start < endIndex)   
+            	quickSort(numbers, start, endIndex); //递归排序前半段  
+        	if (end > startIndex)   
+            	quickSort(numbers, startIndex, end);   //递归排序后半段
+    	}   
+	}  
 }
